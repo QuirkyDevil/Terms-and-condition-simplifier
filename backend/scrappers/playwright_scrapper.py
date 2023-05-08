3df24eb0df95b4ec09e28c015634b40abc46aa62
QuirkyDevil <sohamrupaye123@gmail.com>
2023-05-08 12:27:08 +0530

    [HOTFIX] scrapper changes

--- backend/scrappers/playwright_scrapper.py
+++ backend/scrappers/playwright_scrapper.py
@@ -41,43 +41,24 @@ async def scrape_website(browser, url: str) -> tuple | int:
 
 
 async def scrape(browser, company: str) -> tuple | int:
-    """Scrape the website and return the text of the website"""
     page = await browser.new_page()
-
-    # Try to navigate to the company's terms and conditions page
-    try:
-        await page.goto(BASE_URL + company + TERMS_AND_CONDITIONS, wait_until="commit")
-    except Exception:
-        await page.close()
-        return 500
-
-    # Get the text of the link
+    await page.goto(BASE_URL + company + TERMS_AND_CONDITIONS, wait_until="commit")
     text_of_link = await page.inner_text("xpath=//h3[@class='LC20lb MBeuO DKV0Md']")
 
-    # Check if the text of the link matches any of the keywords
     if any(keyword in text_of_link.lower() for keyword in URL_KEYWORDS):
-        try:
-            # Click on the link and wait for the page to load
-            await page.click("xpath=//h3[@class='LC20lb MBeuO DKV0Md']")
-            await page.wait_for_load_state("domcontentloaded")
-
-            # Get the text and link of the page
-            text = await page.evaluate("document.body.innerText")
-            link = await page.evaluate("document.URL")
-        except Exception:
-            await page.close()
-            return 500
-
-        # Get only the sentences from the text
+        await page.click("xpath=//h3[@class='LC20lb MBeuO DKV0Md']")
+        # get link of the site
+        await page.wait_for_load_state("domcontentloaded")
+        #  some javascript thing to get some information about the site
+        text = await page.evaluate("document.body.innerText")
+        link = await page.evaluate("document.URL")
+        # get all the sentences from the text
         text = re.findall(SENTECE_RX, text)
         text = "\n".join(text)
 
-        # Check if the connection is not private or the site is not reachable
-        if CONNECTION_NOT_PRIVATE or SITE_NOT_REACHABLE in text:
+        if CONNECTION_NOT_PRIVATE in text or SITE_NOT_REACHABLE in text:
             await page.close()
             return 500
-
-        # Close the page
         await page.close()
         return (text, link)
     else:
@@ -94,3 +75,5 @@ if __name__ == "__main__":
         print(text)
 
     asyncio.run(main())
+
+
