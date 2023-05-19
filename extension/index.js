ff0401856474530506b6635a596adae6b1e4c3b9
shreyas phadnis <phadnis.shreyas@gmail.com>
2023-05-20 02:06:23 +0530

    reduced code length and added functions for reusablity

--- extension/index.js
+++ extension/index.js
@@ -60,20 +60,93 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("jokes").style.display = "none";
   }
 
-  hideLoadingAnimation();
+  let errorProperties = {
+    errorMessage: "Please double-check the company name and try again",
+    errorImage: "./logos/file_error.png",
+    errorJokeImage:
+      "https://media.tenor.com/6PjP_7dSl4QAAAAC/akshay-kumar-dizziness.gif",
 
-  searchButton.addEventListener("click", function () {
-    let company_name = getCompanyName();
+    errorJokeMessage:
+      "Oops! Did you search for Laxmi Chit Fund or do we have high traffic? Please try again :)",
+  };
 
-    bodyEle.style.display = "none";
+  let errorDesign = (message) => {
+    let errorDiv = document.createElement("div");
+    document.body.style.overflow = "hidden";
 
-    showLoadingAnimation();
+    let img = document.createElement("img");
+    img.src = errorProperties.errorImage;
+    img.height = "90";
+    img.width = "90";
 
-    fetch(root_link + get_summary + "?company=" + company_name)
+    let title = document.createElement("p");
+    title.innerText = "Error!";
+    title.style.textAlign = "center";
+    title.style.fontWeight = "bold";
+    title.style.marginTop = "50px";
+
+    var text = document.createElement("p");
+    text.innerText = message;
+    text.style.textAlign = "center";
+    text.style.color = "#888888";
+
+    let errorText = document.createElement("div");
+
+    errorText.appendChild(title);
+    errorText.appendChild(text);
+
+    let tryAgainButton = document.createElement("button");
+    tryAgainButton.style.margin = "20px";
+    tryAgainButton.textContent = "Try again?";
+    tryAgainButton.style.width = "60%";
+    tryAgainButton.style.backgroundColor = "#00acee";
+    tryAgainButton.style.borderRadius = "30px";
+    tryAgainButton.style.color = "white";
+    tryAgainButton.style.fontWeight = "bold";
+    tryAgainButton.style.border = "none";
+    tryAgainButton.style.height = "40px";
+    tryAgainButton.style.cursor = "pointer";
+
+    tryAgainButton.addEventListener("click", () => {
+      let company_name = getCompanyName();
+      summary.style.display = "none";
+      showLoadingAnimation();
+      fetchData(get_summary, "?company=", company_name);
+    });
+
+    errorDiv.appendChild(img);
+    errorDiv.appendChild(errorText);
+    errorDiv.appendChild(tryAgainButton);
+
+    errorDiv.style.display = "flex";
+    errorDiv.style.alignItems = "center";
+    errorDiv.style.justifyContent = "center";
+    errorDiv.style.flexDirection = "column";
+
+    errorDiv.style.position = "absolute";
+
+    // errorDiv.style.top = "50%";
+    // errorDiv.style.left = "50%";
+    // errorDiv.style.transform = "translate(-50%, -50%)";
+    errorDiv.style.height = "70%"; // Set height to 70%
+    errorDiv.style.width = "70%";
+
+    summary.appendChild(errorDiv);
+    summary.style.height = "80vh";
+
+    summary.style.display = "flex";
+    summary.style.alignItems = "center";
+    summary.style.justifyContent = "center";
+    summary.style.overflow = "hidden";
+  };
+
+  let fetchData = (endPoint, parameter, company) => {
+    fetch(root_link + endPoint + parameter + company)
       .then((res) => {
         hideLoadingAnimation();
         if (!res.ok) {
           throw new Error("network response was not ok");
+          errorDesign("There was an internet connectivity issue");
         }
 
         return res.json();
@@ -86,163 +159,40 @@ document.addEventListener("DOMContentLoaded", () => {
       })
       .catch((err) => {
         hideLoadingAnimation();
-        //   let errorDiv = document.createElement("div");
-        //   var img = document.createElement("img");
-        //   img.src = "./logos/file_error.png";
-        //   img.height = "70";
-        //   img.width = "70";
-        //   img.alt = "Error";
-
-        //   var text = document.createElement("p");
-        //   text.innerText =
-        //     "Oops! Did you search for Laxmi Chit Fund or do we have high traffic. Please try again :)";
-        //   text.style.textAlign = "center";
-
-        //   errorDiv.appendChild(img);
-        //   errorDiv.appendChild(text);
-        //   summary.appendChild(errorDiv);
-        //   summary.style.backgroundColor = "red";
-
-        //   summary.style.height = "100%";
-        //   summary.style.width = "100%";
-        //   summary.style.display = "flex";
-        //   summary.style.alignItems = "center";
-        //   summary.style.justifyContent = "center";
-
-        //   errorDiv.style.display = "flex";
-        //   errorDiv.style.alignItems = "center";
-        //   errorDiv.style.justifyContent = "center";
-        //   errorDiv.style.flexDirection = "vertical";
-        //   errorDiv.style.backgroundColor = "yellow";
-        //   errorDiv.style.height = "100%";
-        //   errorDiv.style.width = "100%";
-
-        let errorGif =
-          "https://media.tenor.com/6PjP_7dSl4QAAAAC/akshay-kumar-dizziness.gif";
-
-        let errorJokeMessage =
-          "Oops! Did you search for Laxmi Chit Fund or do we have high traffic? Please try again :)";
-        let errorMessage = "Please double-check the company name and try again";
-        let errorImage = "./logos/file_error.png";
-
-        let errorDiv = document.createElement("div");
-        document.body.style.overflow = "hidden";
-
-        let img = document.createElement("img");
-        img.src = errorImage;
-        img.height = "90";
-        img.width = "90";
-
-        let title = document.createElement("p");
-        title.innerText = "Error!";
-        title.style.textAlign = "center";
-        title.style.fontWeight = "bold";
-        title.style.marginTop = "50px";
-
-        var text = document.createElement("p");
-        text.innerText = errorMessage;
-        text.style.textAlign = "center";
-        text.style.color = "#888888";
-
-        let errorText = document.createElement("div");
-
-        errorText.appendChild(title);
-        errorText.appendChild(text);
-
-        let tryAgainButton = document.createElement("button");
-        tryAgainButton.style.margin = "20px";
-        tryAgainButton.textContent = "Try again?";
-        tryAgainButton.style.width = "60%";
-        tryAgainButton.style.backgroundColor = "#00acee";
-        tryAgainButton.style.borderRadius = "30px";
-        tryAgainButton.style.color = "white";
-        tryAgainButton.style.fontWeight = "bold";
-        tryAgainButton.style.border = "none";
-        tryAgainButton.style.height = "40px";
-
-        errorDiv.appendChild(img);
-        errorDiv.appendChild(errorText);
-        errorDiv.appendChild(tryAgainButton);
-
-        errorDiv.style.display = "flex";
-        errorDiv.style.alignItems = "center";
-        errorDiv.style.justifyContent = "center";
-        errorDiv.style.flexDirection = "column";
-
-        errorDiv.style.position = "absolute";
-
-        // errorDiv.style.top = "50%";
-        // errorDiv.style.left = "50%";
-        // errorDiv.style.transform = "translate(-50%, -50%)";
-        errorDiv.style.height = "70%"; // Set height to 70%
-        errorDiv.style.width = "70%";
-
-        summary.appendChild(errorDiv);
-        summary.style.height = "80vh";
-
-        summary.style.display = "flex";
-        summary.style.alignItems = "center";
-        summary.style.justifyContent = "center";
-        summary.style.overflow = "hidden"; // Disable scrolling
+        errorDesign(errorProperties.errorMessage);
       });
+  };
+
+  hideLoadingAnimation();
+
+  searchButton.addEventListener("click", function () {
+    let company_name = getCompanyName();
+
+    bodyEle.style.display = "none";
+
+    showLoadingAnimation();
+    fetchData(get_summary, "?company=", company_name);
   });
 
   function staticCompanies(element, company) {
     element.addEventListener("click", () => {
       bodyEle.style.display = "none";
-      fetch(root_link + get_summary + "?company=" + company)
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error("network response was not ok");
-          }
 
-          return res.json();
-        })
-        .then((data) => {
-          // add title to the summary
-          const title = document.createElement("h1");
-          title.innerHTML = company;
-          summary.appendChild(title);
-          // add summary to the summary
-
-          let dataParam = data.data.summary;
-
-          const summaryText = dataParam.split("\n").join("<br /><br />");
-          summary.innerHTML = summaryText;
-        })
-        .catch((err) => {
-          p;
-          summary.innerHTML = `There was an error with the fetch operation ${err}`;
-        });
+      showLoadingAnimation();
+      fetchData(get_summary, "?company=", company);
     });
   }
-
-  staticCompanies(googleBtn, "google");
-  staticCompanies(facebookBtn, "facebook");
-  staticCompanies(twitterBtn, "twitter");
-  staticCompanies(microsoftBtn, "microsoft");
 
   userButton.addEventListener("click", () => {
     const text = textarea.value;
 
     bodyEle.style.display = "none";
-
-    fetch(root_link + user_summary + "?text=" + text)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("network response was not ok");
-        }
-
-        return res.json();
-      })
-      .then((data) => {
-        let dataParam = data.data.summary;
-        const summaryText = dataParam.split("\n").join("<br /><br />");
-
-        summary.innerHTML = summaryText;
-      })
-      .catch((err) => {
-        summary.innerHTML = `There was an error with the fetch operation ${err}`;
-      });
+    showLoadingAnimation();
+    fetchData(user_summary, "?text=", text);
   });
+
+  staticCompanies(googleBtn, "google");
+  staticCompanies(facebookBtn, "facebook");
+  staticCompanies(twitterBtn, "twitter");
+  staticCompanies(microsoftBtn, "microsoft");
 });
